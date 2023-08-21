f7927410893c9c40ad0116a1f84f37067d8d427a
Hamza Ashfaq <hamza.ashfaque2012@gmail.com>
2023-08-21 23:20:23 +0500

    test

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false
 };
-export const apiUrl = 'http://localhost:8080';
+export const apiUrl = 'http://localhost:8080/api';
 
 /*
  * For easier debugging in development mode, you can import the following file
